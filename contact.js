88ee6df5009e59574dbb4fe55a9f1cda818598ca
Klein Maetschke <kleinmaetschke@gmail.com>
2025-03-03 14:03:21 -0500

    Adjusting reCaptcha method

--- contact.js
+++ contact.js
@@ -1,14 +1,8 @@
-function onSubmit(token) {
-  const email = "heyfutureghosts@gmail.com";
-  document.getElementById("contact-link").href = `mailto:${email}`;
-  document.getElementById("contact-link").click();
-}
+document.getElementById('contact-link').addEventListener('click', function(event) {
+  event.preventDefault();
+  grecaptcha.execute();
+});
 
-document
-  .getElementById("contact-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    grecaptcha.execute().catch(function (error) {
-      console.error("Error executing reCAPTCHA:", error);
-    });
-  });
+function onSubmit(token) {
+  window.location.href = 'mailto:heyfutureghosts@gmail.com';
+}
