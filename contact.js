a6a26c6ad226f749704aa318012401eb824bf227
Klein Maetschke <kleinmaetschke@gmail.com>
2025-03-03 13:36:28 -0500

    Pushing initial site.

---
+++ contact.js
@@ -0,0 +1,12 @@
+function onSubmit(token) {
+  const email = "heyfutureghosts@gmail.com";
+  document.getElementById("contact-link").href = `mailto:${email}`;
+  document.getElementById("contact-link").click();
+}
+
+document
+  .getElementById("contact-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    grecaptcha.execute();
+  });
