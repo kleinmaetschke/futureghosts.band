e3e7a26351508623a90a12955589c3b7982e8096
Klein Maetschke <kleinmaetschke@gmail.com>
2025-03-04 11:23:39 -0500

    New ReCaptcha tag

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 document.getElementById('contact-link').addEventListener('click', function(event) {
   event.preventDefault();
   grecaptcha.ready(function() {
-    grecaptcha.execute('6LcUgOgqAAAAAKRoAiQGaPOqPuY1bPQ46SlhF3Jv', {action: 'submit'}).then(function(token) {
+    grecaptcha.execute('6LeLdukqAAAAAGsZeN5CLTMTJY5bvBnEpMOuzd29', {action: 'submit'}).then(function(token) {
       onSubmit(token);
     });
   });
